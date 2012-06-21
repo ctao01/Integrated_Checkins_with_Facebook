3c64fa0da0b22aca126c719beb90da65cc158110
joycytao <ctao01.poly@gmail.com>
2012-06-21 10:17:15 -0500

    Initial Commit

---
+++ jCalendarView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  jCalendarView
+//
+//  Created by Joy Tao on 6/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
